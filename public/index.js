e120162e7d4fd4650b4123279cc818f568374b60
Mohamed Atef <atefhamosa@gmail.com>
2023-05-06 22:25:03 +0300

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,1 +1,30 @@
-console.log("Hello, World!");
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("../routes");
+const bodyParser = require("body-parser");
+const path = require("path");
+var cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+app.use(cors({ origin: "*" }));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(routes);
+
+try {
+  mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+} catch (e) {
+  console.log(e);
+}
+
+app.listen(3000, () => {
+  console.log(`listening on port ${3000}`);
+});
+
+module.exports = app;
