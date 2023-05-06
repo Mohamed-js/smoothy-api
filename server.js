498d16249361d8fe39e37ab03f2c7e0d2a32b333
Mohamed Atef <atefhamosa@gmail.com>
2023-05-06 22:04:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
-const startServer = require("./config");
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const bodyParser = require("body-parser");
+const path = require("path");
+var cors = require("cors");
 
-startServer();
+require("dotenv").config();
+
+const app = express();
+app.use(cors({ origin: "*" }));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(routes);
+
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.listen(3000, () => {
+  console.log(`listening on port ${3000}`);
+});
+
+module.exports = app;
